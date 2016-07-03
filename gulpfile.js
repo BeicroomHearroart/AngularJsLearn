eaf43738fd3e6937b4aba6d264a1916a6cd26ff2
BeicroomHearroart <beicroomhearroart@hotmail.com>
2016-07-03 20:17:54 +0800

    update gulp and plugins

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var ugl = require('gulp-uglify');
+var babel = require('gulp-babel');
 
-gulp.task('default', function(){
+gulp.task('testTask', function() {
+    gulp.src('TestPromise/*.js')
+        .pipe(ugl())
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('default', ['testTask'], function(){
     console.log("hello gulp");
 });
