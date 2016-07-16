7c7705754631845d77c878ef580bc182823119b2
BeicroomHearroart <beicroomhearroart@hotmail.com>
2016-07-16 11:15:42 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
 var gulp = require('gulp');
 var ugl = require('gulp-uglify');
 var babel = require('gulp-babel');
+var fs = require('graceful-fs');
+var del = require('del');
 
-gulp.task('testTask', function() {
-    gulp.src('TestPromise/*.js')
-        .pipe(ugl())
+gulp.task('testTask', ['clean'], function() {
+    gulp.src('TestPromise/*.html')
+        //.pipe(ugl())
         .pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', ['testTask'], function(){
-    console.log("hello gulp");
-});
+gulp.task('build', ['testTask']);
+
+gulp.task('clean', function() {
+    return del(['dist/*.js'])
+        .then(function(paths) {
+            console.log('Deleted files and folders: ', paths.join('\n'))
+        });
+})
