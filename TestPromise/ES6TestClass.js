490db9342c4b9112a4264cb833c9aea928c61a4e
BeicroomHearroart <beicroomhearroart@hotmail.com>
2016-07-23 00:15:03 +0800

    Add webpack

---
+++ TestPromise/ES6TestClass.js
@@ -0,0 +1,11 @@
+export class ES6TestClass {
+    constructor(){
+        this.init();
+    }
+
+    init() {
+        let name = "Beic";
+
+        console.log(name);
+    }
+}
--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,41 @@ var gulp = require('gulp');
 var ugl = require('gulp-uglify');
 var babel = require('gulp-babel');
 var fs = require('graceful-fs');
+var notify = require('gulp-notify');
 var del = require('del');
+
+var i = 0;
 
 gulp.task('testTask', ['clean'], function() {
     gulp.src('TestPromise/*.html')
+        .pipe(gulp.dest('dist'));
+
+    gulp.src('TestPromise/*.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(notify({ message: 'babel task complete' }))
         //.pipe(ugl())
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', ['testTask']);
 
 gulp.task('clean', function() {
-    return del(['dist/*.js'])
-        .then(function(paths) {
-            console.log('Deleted files and folders: ', paths.join('\n'))
-        });
-})
+    del(['dist/*.*']);
+
+    gulp.src('TestPromise/*.js').pipe(notify({ message: 'clean task complete for ' + i }));
+
+        // .then(function(paths) {
+        //     console.log('Deleted files and folders: ', paths.join('\n'))
+        // });
+});
+
+// gulp.task('trans', function() {
+//     return gulp.src('TestPromise/ES6TestClass.js')
+//         .pipe(babel({
+//             presets: ['es2015']
+//         }))
+//         .pipe(gulp.dest('dist'))
+//         .pipe(notify({ message: 'babel task complete' }));
+// });
